d94273ed2a2d856413fa01365ff17fc35d8a44ac
aksel-r <yassinrezgui19gmail.com>
2025-03-16 07:39:56 +0100

    added animefy API

--- streamVerse-backEnd/routes/anime/streamAnimeEpisode.js
+++ streamVerse-backEnd/routes/anime/streamAnimeEpisode.js
@@ -3,11 +3,12 @@ var router = express.Router();
 const { streamAnimeEpisode } = require("../../controller/anime/streamAnimeEpisode");
 
 
-router.get('/:query/:season/:episode', async function (req, res, next) {
+router.get('/:name/:query/:season/:episode', async function (req, res, next) {
   const query = req.params.query;
   const season = req.params.season;
   const episode = req.params.episode;
-  let results = await streamAnimeEpisode(query,season,episode);
+  const name = req.params.name;
+  let results = await streamAnimeEpisode(query,season,episode,name);
   res.send(results);
 });
 
